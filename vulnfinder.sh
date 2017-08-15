48920e50dd1591420266e41ab5862567b21f0c99
msmith <msmith@continuum.io>
2017-08-15 12:20:49 -0400

    cleanup

--- vulnfinder.sh
+++ vulnfinder.sh
@@ -16,10 +16,6 @@ fi
 
 export TDIR=`python ./mktempfile.py`
 
-echo $LIBSUFFIX
-echo $TDIR
-
-
 # Find top-level modules
 # Query: include sub-module names too?
 find $SP -name "__init__.py" \
@@ -44,12 +40,10 @@ conda list -p  $1 \
     | grep -v '#' \
     > $TDIR/packages.txt
 
-echo $TDIR
-
 python vulndigester.py -d -i ignore-words.txt --env $1 $2 $TDIR/packages.txt $TDIR/libraries.txt $TDIR/modules.txt
 
 # vulndigester.py has a number of options
 # python vulndigester.py -h will explain them, a bit tersely
 
-#rm -rf $TDIR
+rm -rf $TDIR
 
